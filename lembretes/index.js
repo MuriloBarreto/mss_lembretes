652095cda447ed23ffa18d9795fa3bfb7d786531
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-03-15 21:53:52 -0300

    Criacao inicial do mss de lembretes

---
+++ lembretes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/lembretes', (req, res) => {
+
+})
+
+app.put ('/lembretes', (req, res) =>{
+
+})
+
+app.listen(4000, () => console.log("Lembretes:4000"));
